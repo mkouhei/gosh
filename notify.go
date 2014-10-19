3b2397cb25df68bd3ad97692f7cd6cd39ead12b2
Kouhei Maeda <mkouhei@palmtb.net>
2014-10-19 09:02:49 +0900

    Added notify IS_MODIFY.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- notify.go
+++ notify.go
@@ -3,27 +3,48 @@ package main
 import (
 	"fmt"
 
+	"path/filepath"
+	"sync/atomic"
+
 	"github.com/howeyc/fsnotify"
 )
 
+const (
+	tmpname = "tmpcode"
+)
+
+type cnt struct {
+	val int32
+}
+
+func (c *cnt) incremant() {
+	atomic.AddInt32(&c.val, 1)
+}
+
 func watch(targetDir string) error {
+	tmpFile := fmt.Sprintf("%s/%s", targetDir, tmpname)
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		fmt.Println("new watcher error:", err)
 		return err
 	}
+	var modifyRecieved cnt
 	done := make(chan bool)
 
 	go func() {
-		for {
-			select {
-			case ev := <-watcher.Event:
-				fmt.Println("event: ", ev)
-			case err := <-watcher.Error:
-				fmt.Println("error: ", err)
-				break
+		for event := range watcher.Event {
+			fmt.Println(event)
+			if event.Name == filepath.Clean(tmpFile) {
+				fmt.Printf("event recieved: %s", event)
+				if event.IsModify() {
+					modifyRecieved.incremant()
+				}
+			} else {
+				fmt.Printf("unexpected event recieved: %s", event)
 			}
 		}
+		done <- true
 	}()
 
 	err = watcher.Watch(targetDir)
--- notify_test.go
+++ notify_test.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestWatch(t *testing.T) {
 
 	blddir := bldDir()
-	tmpFile := fmt.Sprintf("%s/%s", blddir, "testcode")
+	tmpFile := fmt.Sprintf("%s/%s", blddir, "tmpcode")
 
 	go func() {
 		if err := watch(blddir); err != nil {
@@ -17,9 +18,17 @@ func TestWatch(t *testing.T) {
 		}
 	}()
 
-	err := ioutil.WriteFile(tmpFile, []byte{}, 0644)
+	fmt.Println("[test create]")
+	f, err := os.OpenFile(tmpFile, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("creating test file faild: %s", err)
 	}
+	f.Sync()
+
+	time.Sleep(time.Microsecond)
+	fmt.Println("[test modify]")
+	f.WriteString("dummy")
+	f.Sync()
+	f.Close()
 
 }
