36173a59351419e11748ccda055dc8839500e031
Kouhei Maeda <mkouhei@palmtb.net>
2014-10-26 08:15:22 +0900

    Added writer.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ write.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"time"
+)
+
+func writeFile(codePath string, content string) error {
+	f, err := os.OpenFile(codePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+	time.Sleep(time.Microsecond)
+
+	f.WriteString(content)
+	f.Sync()
+	f.Close()
+	return nil
+}
